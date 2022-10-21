a5fe052827c4957a9d71886479d58f568bf50355
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-21 16:43:45 -0300

    feat: ChannelsListData added

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,24 @@ const gamesListData = [
   }
 ]
 
+const channelsListData = [
+  {
+    url: "https://www.twitch.tv/maykbrito",
+    imageUrl: "https://static-cdn.jtvnw.net/jtv_user_pictures/9ce11a2b-ec84-44b1-9c76-b8d29df5fef0-profile_image-150x150.png",
+    alt: "Imagem de Mayk Brito"
+  },
+  {
+    url: "https://www.twitch.tv/alanzoka",
+    imageUrl: "https://static-cdn.jtvnw.net/jtv_user_pictures/15cec952-c1ba-4ff8-a79c-53c2fa5bd269-profile_image-150x150.png",
+    alt: "Imagem de Alanzoka"
+  },
+  {
+    url: "https://www.twitch.tv/cellbit",
+    imageUrl: "https://static-cdn.jtvnw.net/jtv_user_pictures/cb885149-f7f7-48f0-99ae-fe0a386ed3e2-profile_image-150x150.png",
+    alt: "Imagem de Cellbit"
+  }
+]
+
 function App() {
   return (
     <div className="App">
@@ -37,7 +55,13 @@ function App() {
             )
           })}
         </Section>
-        <Section title="Canais e streamers" subtitle="Lista de canais e transmissões que não perco!" />
+        <Section title="Canais e streamers" subtitle="Lista de canais e transmissões que não perco!">
+          {channelsListData.map((channel) => {
+              return (
+                <ListItem url={channel.url} imageUrl={channel.imageUrl} alt={channel.alt}/>
+              )
+          })}
+        </Section>
       </main>
     </div>
   )
