a89c49ba112b8f3e83a94b9349cb0ba7b920be8a
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-21 11:34:55 -0300

    feat: section component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from './components/Header'
+import { Section } from './components/Section'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <main>
+        <Section />
+      </main>
     </div>
   )
 }
